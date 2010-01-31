ef1209b6173da82fac5600aaaf5b38ff8856e78a
Kevin Risden <compuwizard123@gmail.com>
2010-01-31 14:36:09 -0500

    Added placeholder methods. To finish later

--- src/SkipList.java
+++ src/SkipList.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
 
 public class SkipList<T extends Comparable<? super T>> {
+	private Node root = null;
 
+	public SkipList() {
+
+	}
+
+	public SkipList(int[] start) {
+
+	}
+
+	public boolean insert(T item) {
+		return true;
+	}
+
+	public boolean remove(T item) {
+		return true;
+	}
+
+	public int getNodesVisited() {
+		return 0;
+	}
+
+	public Node getRoot() {
+		return this.root;
+	}
+
+	public class Node {
+		private T element = null;
+
+		public Node() {
+
+		}
+
+		public ArrayList<Node> getLinks() {
+			return new ArrayList<Node>();
+		}
+
+		public T getElement() {
+			return element;
+		}
+
+	}
 }
